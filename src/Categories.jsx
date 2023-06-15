609a26a522d9a4470bd537804977a812f2bdd271
annamaynard <anna.maynard@gmail.com>
2023-06-15 10:58:45 -0400

    june15

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import he from 'he'
+
 
 
 const Categories = ({setSelectedCategory}) => {
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Category from "./Categories";
 import he from "he";
 import arrayShuffle from "array-shuffle";
 
